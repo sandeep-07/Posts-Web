4b3db8d355c71639096b9f84d5226d29dc388818
sandeep-07 <sandeepisthename007@gmail.com>
2021-09-04 01:26:57 +0530

    added immer

--- src/redux/create/CreateReducer.js
+++ src/redux/create/CreateReducer.js
@@ -1,11 +1,18 @@
+import React from "react";
 import { CREATE_POST } from "./CreateTypes";
-
+import { produce } from "immer";
 const createdPosts = [];
+
+export function useImerReducer(reducer, initialState) {
+  return React.useReducer(produce(reducer), initialState);
+}
 
 const CreateReducer = (state = createdPosts, action) => {
   switch (action.type) {
     case CREATE_POST:
-      return [...state, action.payload];
+      return produce(state, (drafState) => {
+        drafState.unshift(action.payload);
+      });
     default:
       return state;
   }
